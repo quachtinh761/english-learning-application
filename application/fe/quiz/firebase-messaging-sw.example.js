5618bf9377cd75187fd8eb2f1281e74c62ee64ae
Tino Nguyen <quachtinh761@gmail.com>
2024-09-23 08:17:25 +0700

    Add Demo UI

---
+++ application/fe/quiz/firebase-messaging-sw.example.js
@@ -0,0 +1,27 @@
+// Import Firebase libraries
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
+    console.log('Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
